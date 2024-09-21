a6ff82fd2a3bbdf2eb1c748a99fad51ab17b95bf
kernel10452 <162881542+kernel10452@users.noreply.github.com>
2024-09-21 03:09:36 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="background">
+      <div className="panel">
+        <br></br>
+        <p><b>ستضحكون قليلاً وتبكون كثيرًا، لأنكم لا تعرفون أي الخطوط الحمراء قد تجاوزتموها</b></p>
+        <br></br>
+        <p><b>אתם תצחקו קצת ותבכו הרבה, כי אתם לא יודעים אילו קווים אדומים חציתם</b></p>
+        <br></br>
+        <p><b>You will laugh a little and cry a lot, as you do not know which redlines you have crossed</b></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
